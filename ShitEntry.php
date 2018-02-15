5736fa29911225ed89a6053d9a0f8c674bbffd6d
Unknown <dan.c.cook@gmail.com>
2018-02-15 15:19:24 -0600

    Updated template path and filenames

--- ShitEntry.php
+++
@@ -1,8 +0,0 @@
-<div class="Date"><?php echo $shitDate?></div>
-<div class="flex-container Entry">
-    <div class="Stamp">
-        <div class="Time"><?php echo $shitTime?></div>
-        <div class="User"><?php echo $shitUser?>:</div>
-    </div>
-    <div class="Shitpost"><?php echo $shitPost?></div>
-</div>
